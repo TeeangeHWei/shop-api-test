379be43daeabe6f99937b7939c83d60844436f83
CYRUS <>
2022-03-31 13:50:19 +0800

    123

--- user-web/main.go
+++ user-web/main.go
@@ -6,16 +6,15 @@ import (
 	"shop-api/user-web/initialize"
 )
 
-func main()  {
+func main() {
 	// 1. 初始化router
 	Router := initialize.Routers()
 
-
-	zap.S().Info("启动服务器，端口:%d",8021)
+	zap.S().Info("启动服务器，端口:%d", 8022)
 
 	port := 8021
-	if err := Router.Run(fmt.Sprintf(":%d",port));err != nil{
-		zap.S().Panic("启动失败",err.Error())
+	if err := Router.Run(fmt.Sprintf(":%d", port)); err != nil {
+		zap.S().Panic("启动失败", err.Error())
 	}
 
 }
