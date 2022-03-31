e8070349bd16faeae63f527e55e1d086872eb6a6
CYRUS <>
2022-03-31 13:49:44 +0800

    Initial commit

---
+++ user-web/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"go.uber.org/zap"
+	"shop-api/user-web/initialize"
+)
+
+func main()  {
+	// 1. 初始化router
+	Router := initialize.Routers()
+
+
+	zap.S().Info("启动服务器，端口:%d",8021)
+
+	port := 8021
+	if err := Router.Run(fmt.Sprintf(":%d",port));err != nil{
+		zap.S().Panic("启动失败",err.Error())
+	}
+
+}
